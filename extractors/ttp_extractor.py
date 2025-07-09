17008a69d7abb87f19356c72d12277a5b020fb2c
Viktor Lindén <vilinden@ug.kth.se>
2025-07-09 17:42:17 +0200

    Fixed mitre phase order

--- extractors/ttp_extractor.py
+++ extractors/ttp_extractor.py
@@ -24,7 +24,16 @@ def extract_ttps(text, console):
             ttps.append({'type': 'TTP', 'value': normalized, 'name': info['name'], 'phase': info['phase'], 'ref': info['ref']})
             seen.add(normalized)
 
-    return sorted(ttps, key=lambda x: (x['phase'], x['name']))
+    phase_order = [
+        "reconnaissance", "resource-development", "initial-access", "execution", "persistence",
+        "privilege-escalation", "defense-evasion", "credential-access", "discovery",
+        "lateral-movement", "collection", "command-and-control", "exfiltration", "impact"
+    ]
+    
+    return sorted(
+                ttps,
+                key=lambda x: (phase_order.index(x['phase']), x['name'])
+            )
 
 def get_ttp_info(ttp, mitre_attack_data) -> dict:
     info = mitre_attack_data.get_object_by_attack_id(ttp, "attack-pattern")
